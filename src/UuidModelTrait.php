e0fcae5fe8df791c9d1b29f27d28d7505453ac9b
Alex Sofronie <alsofronie@gmail.com>
2015-11-17 01:06:25 +0200

    phpdocs

--- src/UuidModelTrait.php
+++ src/UuidModelTrait.php
@@ -4,13 +4,26 @@ namespace Alsofronie\Uuid;
 
 use Webpatser\Uuid\Uuid;
 
+/*
+ * @package Alsofronie\Uuid
+ * @author Alex Sofronie <alsofronie@gmail.com>
+ * @license MIT
+ */
 trait UuidModelTrait
 {
+	/*
+	 * This function is used internally by Eloquent models to test if the model has auto increment value
+	 * @returns bool Always false
+	 */
     public function getIncrementing()
     {
         return false;
     }
 
+    /**
+     * This function overwrites the default boot static method of Eloquent models. It will hook
+     * the creation event with a simple closure to insert the UUID
+     */
     public static function boot() {
 
     	parent::boot();
