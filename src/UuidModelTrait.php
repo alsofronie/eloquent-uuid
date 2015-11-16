3eb2aa5ffbb2f50c2e4eca7cfd562f1d70bf4775
Alex Sofronie <alsofronie@gmail.com>
2015-11-17 00:21:35 +0200

    Initial commit

---
+++ src/UuidModelTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Alsofronie\Uuid;
+
+use Webpatser\Uuid\Uuid;
+
+trait UuidModelTrait
+{
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public static function boot() {
+        static::creating(function($model) {
+            $model->attributes[$model->getKeyName()] = str_replace('-','', Uuid::generate(4));
+        }, 0);
+    }
+
+}
